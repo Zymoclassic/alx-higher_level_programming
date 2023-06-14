f3e31feb7002740eec2bbbc03a7b02b8c297df1c
zymoclassic <awopejumoses@gmail.com>
2023-06-14 08:44:33 -0700

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for x in range(len(matrix)):
+        new_matrix[x] = list(map(lambda i: i**2, matrix[x]))
+
+    return (new_matrix)
